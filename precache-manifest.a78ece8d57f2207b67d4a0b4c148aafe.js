139e08a5b25f02907aa8ce6f8d0bf5e5b538574a
Kee Tee <tkeeching@gmail.com>
2020-10-25 18:00:18 +1100

    Updates

---
+++ precache-manifest.a78ece8d57f2207b67d4a0b4c148aafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c38bdcb92c5f8fec1a494b9f5ed0e65",
+    "url": "/how-far-can-i-go/index.html"
+  },
+  {
+    "revision": "db29136bceb85a6a40e6",
+    "url": "/how-far-can-i-go/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e999be30fac75297e15",
+    "url": "/how-far-can-i-go/static/js/2.296a3493.chunk.js"
+  },
+  {
+    "revision": "a503a3d6faf06974c5ca68a810b18027",
+    "url": "/how-far-can-i-go/static/js/2.296a3493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db29136bceb85a6a40e6",
+    "url": "/how-far-can-i-go/static/js/main.1b7b342a.chunk.js"
+  },
+  {
+    "revision": "af48bca458a44737ebac",
+    "url": "/how-far-can-i-go/static/js/runtime-main.5e3dc932.js"
+  }
+]);
